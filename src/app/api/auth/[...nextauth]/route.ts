2905247f8dcdfef3bbbde7177cd3694faa928983
JP Addison <johnpaddison@gmail.com>
2025-05-29 10:56:01 -0400

    prep for email auth, some quality of life changes before beginning claude code

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,12 @@
 import { handlers } from "~/server/auth";
+// import EmailProvider from "next-auth/providers/email"
 
 export const { GET, POST } = handlers;
+
+// providers: [
+//   EmailProvider({
+//     server: process.env.EMAIL_SERVER,
+//     from: process.env.EMAIL_FROM,
+//     // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+//   }),
+// ],
